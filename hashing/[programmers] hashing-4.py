1c015138aad93421c82da7fd34989483ed0c15a8
HMCHON <49745654+HMCHON@users.noreply.github.com>
2021-03-10 17:03:33 +0900

    Update [programmers] hashing-4.py

--- hashing/[programmers] hashing-4.py
+++ hashing/[programmers] hashing-4.py
@@ -3,8 +3,47 @@ Problem 4 베스트앨범
 
 [문제 요점]
 1. 우선 순위 : 장르 -> 재생 횟수 -> 고유 번호 낮은 print(순
-2. 효율성
+2. defaultdict과 itemgetter 그리고 enumerate
+
+정확성: 100.0
+합계: 100.0 / 100.0
 '''
+
+from collections import defaultdict
+from operator import itemgetter
+
+def solution(genres, plays):   
+    answer = []
+    
+    # 장르 우선순위
+    ndict = defaultdict(lambda : 0)
+    for gen, ply in zip(genres, plays):
+        ndict[gen] += ply #  {'classic': 1450, 'pop': 3100})
+    
+    ndict_list = sorted(ndict.items(), key = itemgetter(1), reverse = True) #[('pop', 3100), ('classic', 1450)]
+    rank = []
+    for i in ndict_list:
+        rank.append(i[0])
+    
+    
+    # 장르 - 최대재생수 - 고유번호 정리
+    array = defaultdict(lambda : [])
+    for i, array_tuple in enumerate(zip(genres, plays)):
+        array[array_tuple[0]].append((array_tuple[1], i))
+    
+    # 결과 출력
+    answer = []
+    for gen in rank:
+        gen_list = sorted(array[gen], key = itemgetter(0), reverse = True)
+        # [(2500, 4), (600, 1)], [(800, 3), (500, 0), (150, 2)]
+        
+        if len(gen_list) > 1:
+            answer.append(gen_list[0][1])
+            answer.append(gen_list[1][1])
+        else:
+            answer.append(gen_list[0][1])
+        
+    return answer
 
 
 
@@ -37,6 +76,61 @@ def solution(genres, plays):
         answer.append(plays.index(largest))
         if second != 0:
             answer.append(plays.index(second))
+
+'''
+다른 풀이 [2]
+'''
+from collections import defaultdict
+from operator import itemgetter
+
+def solution(genres, plays):
+    genre_play_dict = defaultdict(lambda: 0)
+    for genre, play in zip(genres, plays):
+        genre_play_dict[genre] += play # defaultdict(<function solution.<locals>.<lambda> at 0x14cc4e638ca0>, {'classic': 1450, 'pop': 3100})
+        
+    genre_rank = [genre for genre, play in sorted(genre_play_dict.items(), key = itemgetter(1), reverse = True)] # ['pop', 'classic']
+    
+    
+    final_dict = defaultdict(lambda : [])
+    for i, genre_play_tuple in enumerate(zip(genres, plays)):
+        final_dict[genre_play_tuple[0]].append((genre_play_tuple[1], i)) # defaultdict(<function solution.<locals>.<lambda> at 0x1528c0fe9d30>, {'classic': [(500, 0), (150, 2), (800, 3)], 'pop': [(600, 1), (2500, 4)]})
+
+
+    answer = []
+    for genre in genre_rank:
+        one_genre_list = sorted(final_dict[genre], key=itemgetter(0), reverse=True)
+        
+        if len(one_genre_list) > 1:
+            answer.append(one_genre_list[0][1])
+            answer.append(one_genre_list[1][1])
+        else:
+            answer.append(one_genre_list[0][1])
+        
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             
 
 
