19080a58c9f8334820be2c3b2a9a8a9e27f93314
HMCHON <49745654+HMCHON@users.noreply.github.com>
2021-03-11 03:12:35 +0900

    Update [programmser] sort-3.py

--- sort/[programmser] sort-3.py
+++ sort/[programmser] sort-3.py
@@ -21,7 +21,20 @@ def solution(cit):
           
 '''
 채점 결과
-정확성: 12.5
-합계: 12.5 / 100.0
+정확성: 100.0
+합계: 100.0 / 100.0
 '''     
 
+def solution(cit):
+    cit.sort() #[0,1,3,5,6]
+    h, k, answer = 0,0,0
+    if cit[0] >= len(cit):
+        return len(cit)
+        
+    for i in range(0,cit[-1]+1):
+       
+        h = sum(j>=i for j in cit) # i번 이상 인용 된 논문 수 구하기 2
+        k = sum(j<=i for j in cit) # i번 이하 인용 된 논문 수 구하기 2
+        if h>=i and k<=i: # 조건만족확인1
+            answer = i
+    return answer
