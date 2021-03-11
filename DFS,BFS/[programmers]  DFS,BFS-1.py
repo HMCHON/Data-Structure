cacaa819d4f9b1c076a81e4a4b0557fd85147830
HMCHON <49745654+HMCHON@users.noreply.github.com>
2021-03-12 02:07:22 +0900

    Create [programmers]  DFS,BFS-1.py

---
+++ DFS,BFS/[programmers]  DFS,BFS-1.py
@@ -0,0 +1,61 @@
+'''
+problem 1 타깃 넘버
+
+문제 요점
+1. dfs, bfs 문제
+2. 완전 탐색 가능
+'''
+
+
+
+# 완전탐색
+from itertools import product
+
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
+ 
+
+
+
+#BFS
+import collections
+
+def solution(numbers, target):
+    answer = 0
+    stack = collections.deque([(0, 0)])
+    while stack:
+        current_sum, num_idx = stack.popleft()
+
+        if num_idx == len(numbers):
+            if current_sum == target:
+                answer += 1
+        else:
+            number = numbers[num_idx]
+            stack.append((current_sum+number, num_idx + 1))
+            stack.append((current_sum-number, num_idx + 1))
+
+    return answer
+
+  
+  
+  
+#DFS
+answer = 0
+def DFS(idx, numbers, target, value):
+    global answer
+    N = len(numbers)
+    if(idx== N and target == value):
+        answer += 1
+        return
+    if(idx == N):
+        return
+
+    DFS(idx+1,numbers,target,value+numbers[idx])
+    DFS(idx+1,numbers,target,value-numbers[idx])
+
+def solution(numbers, target):
+    global answer
+    DFS(0,numbers,target,0)
+    return answer
